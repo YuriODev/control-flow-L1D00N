e3194f90318d663ad9d028b23f41231bdb3354e8
L1D00N <wannabet1faker@gmail.com>
2024-06-03 09:51:52 +0100

    Update exercise_3.py

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,18 @@
 # Your solution to Exercise 3
 
+a = int(input("What number is your bet? "))
+b = a % 2
+if a < 0 or a > 36:
+    print("The bet will not play!")
+elif a == 0:
+    print("Green")
+elif a > 0 and a < 11 or a > 18 and a < 29:
+    if b == 0:
+        print("Black")
+    else:
+        print("Red")
+else:
+    if b == 0:
+        print("Red")
+    else:
+        print("Black")
