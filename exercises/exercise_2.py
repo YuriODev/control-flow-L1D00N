8483f76e4307a998cdd92083e23bc6c3e3e2dbb2
L1D00N <wannabet1faker@gmail.com>
2024-06-03 09:32:40 +0100

    Update exercise_2.py

--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,2 +1,11 @@
 # Your solution to Exercise 2
 
+age = int(input("enter your age "))
+if age <= 1:
+    print("You are an infant. ")
+elif age < 13:
+    print("You are a child. ")
+elif age < 20:
+    print("You are a teenager. ")
+else:
+    print("You are an adult. ")
