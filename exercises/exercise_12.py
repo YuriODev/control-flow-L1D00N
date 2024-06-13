4f07b6c033fc3e8e252fa780ccd2017ea2ab47fb
L1D00N <wannabet1faker@gmail.com>
2024-06-13 11:11:46 +0100

    Update exercise_12.py

--- exercises/exercise_12.py
+++ exercises/exercise_12.py
@@ -1,2 +1,18 @@
 # Your solution to Exercise 12
 
+a = int(input(""))
+
+b = a // 1000
+c = (a // 100) % 10
+d = (a // 10) % 10
+e = a % 10
+
+if b % 2 == 0:
+    b = "*"
+if c % 2 == 0:
+    c = "*"
+if d % 2 == 0:
+    d = "*"
+if e % 2 == 0:
+    e = "*"
+print(f'{b}{c}{d}{e}')
