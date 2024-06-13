d27686aa3203ae0cf15eb8379dad79595831c972
L1D00N <wannabet1faker@gmail.com>
2024-06-13 10:32:50 +0100

    Update exercise_11.py

--- exercises/exercise_11.py
+++ exercises/exercise_11.py
@@ -1,2 +1,4 @@
 # Your solution to Exercise 11
-
+input = int(input("")
+output = "Leap year." if (input % 4 == 0 and input % 100 != 0) or input % 400 == 0 else output = "Ordinary year."
+print(output
