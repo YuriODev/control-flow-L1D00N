c30c72b4b0cae6e3d693177272e85e7a9be1ad5e
L1D00N <wannabet1faker@gmail.com>
2024-06-20 22:29:21 +0100

    Update exercise_17.py

--- exercises/exercise_17.py
+++ exercises/exercise_17.py
@@ -1,2 +1,14 @@
 # Your solution to Exercise 17
 
+a = int(input("please enter a 6 digit number"))
+
+b = a % 10
+c = (a // 10) % 10
+d = (a // 100) % 10
+e = (a // 1000) % 10
+f = (a // 10000) % 10
+g = a // 100000
+if (b + c + d) == (e + f + g):
+    print("Happy")
+else:
+    print("Ordinary")
